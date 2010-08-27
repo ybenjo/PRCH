22c41fc431a1112ed6b3095550201ab0e5712f54
y_benjo <oreno_bukiwo_sitteruka@yahoo.co.jp>
2010-08-27 17:55:43 +0900

    要素が存在しない場合の判定を追加

--- scripts/parse_patent_xml.rb
+++ scripts/parse_patent_xml.rb
@@ -16,13 +16,13 @@ def get_entry(key, year, db_type)
   db = TokyoCabinet::HDB.new
 
   #まずは#{year}.hdbから読み込む
-  db.open("#{DATA_PATH}/db/#{year}_#{db_type}.hdb", HDB::OWRITER | HDB::OCREAT)
+  db.open("#{DATA_PATH}/db/#{year}/#{db_type}.hdb", HDB::OWRITER | HDB::OCREAT)
   value_1 = db.get(key)
   db.close
 
-  #その後#{year}_error.hdbも読み込む
+  #その後#{year}/error.hdbも読み込む
   #valueが存在しない場合nilが返ってくる
-  db.open("#{DATA_PATH}/db/#{year}_#{db_type}_error.hdb", HDB::OWRITER | HDB::OCREAT)
+  db.open("#{DATA_PATH}/db/#{year}/#{db_type}_error.hdb", HDB::OWRITER | HDB::OCREAT)
   value_2 = db.get(key)
   db.close
 
@@ -39,11 +39,11 @@ def put_entry(param)
   
   db = TokyoCabinet::HDB.new
   
-  #error_flagがtrueの場合は#{year}_#{db_type}_error.hdbを開く
+  #error_flagがtrueの場合は#{year}/#{db_type}_error.hdbを開く
   if error_flag
-    db.open("#{DATA_PATH}/db/#{year}_#{db_type}_error.hdb", HDB::OWRITER | HDB::OCREAT)
+    db.open("#{DATA_PATH}/db/#{year}/#{db_type}_error.hdb", HDB::OWRITER | HDB::OCREAT)
   else
-    db.open("#{DATA_PATH}/db/#{year}_#{db_type}.hdb", HDB::OWRITER | HDB::OCREAT)    
+    db.open("#{DATA_PATH}/db/#{year}/#{db_type}.hdb", HDB::OWRITER | HDB::OCREAT)    
   end
 
   db.put(key, value)
@@ -65,44 +65,67 @@ def parse_and_put_xml(year, file_name)
   #kindが特許公報で無い場合falseを返して終了
   kind = get_kind(year, file_name)
   return false if kind !~ /特許公報/
-    
+
+  #訂正情報の判定
   error_flag = kind =~ /訂正/ ? true : false
   
   open("#{DATA_PATH}/#{year}/#{file_name}"){|f|
     xml = Hpricot(f.read)
     
     #訂正公報の場合、"publication-reference"/"document-id"/"doc-number"が2つ存在する
-    #特許番号、本文、IPCのmain-clsfを取得
+    #特許番号、本文、タイトル、IPCのmain-clsfを取得
     #ipcは先頭三文字(ex. A01)までを取得する(これはクラスと呼ばれる分類)
     patent_id = (xml/"publication-reference"/"document-id"/"doc-number").first.inner_text.to_i
+    title = (xml/"invention-title").first.inner_text.toutf8
     body = (xml/"description").inner_text.toutf8
     ipc = (xml/"classification-ipc"/"main-clsf").inner_text.toutf8[0..2]
 
     #app_id|nameを取得
     #これは特許を申請した会社のid|name
     #idは空の場合nilでは無く謎スペースが入っているので、スペースがマッチしたらnilを返しておく
-    app_id = (xml/"applicant"/"registered-number").first.inner_text.toutf8
-    app_id = nil if app_id[0] == " "
-    app_name = (xml/"applicant"/"name").first.inner_text.toutf8
+    #app_id|nameが存在しない場合があるのでその判定もついでに行う
+    tmp_id = (xml/"applicant"/"registered-number")
+    if tmp_id.size > 0
+      app_id = tmp_id.first.inner_text.toutf8
+      app_id = nil if app_id[0] == " "
+      app_name = (xml/"applicant"/"name").first.inner_text.toutf8
+    else
+      app_id = nil
+    end
 
     #tokyocabinetへの書き込み
     #本来ならネストしたhashをMarshal.dumpするところだがC/C++で叩く予定があるので
     #今回は属性ごとに別のデータベースファイルを作る。非常に煩雑
+    put_entry :key => patent_id, :value => title, :type => "title", :year => year, :flag => error_flag
     put_entry :key => patent_id, :value => body, :type => "body", :year => year, :flag => error_flag
     put_entry :key => patent_id, :value => ipc, :type => "ipc", :year => year, :flag => error_flag
-    put_entry :key => patent_id, :value => app_name, :type => "app_name", :year => year, :flag => error_flag
 
-    #app_idがnilで場合で無い場合のみ、nameとidを紐付ける
-    put_entry :key => app_name, :value => app_id, :type => "app_name", :year => year, :flag => error_flag if !app_id.nil?
+    #app_idがnilで場合で無い場合のみ、app_name => app_id 及び patent_id => app_name を紐付ける
+    if !app_id.nil?
+      put_entry :key => app_name, :value => app_id, :type => "app_name", :year => year, :flag => error_flag 
+      put_entry :key => patent_id, :value => app_name, :type => "app_name", :year => year, :flag => error_flag
+    end
+    
+    #訂正の場合、訂正理由を残しておく
+    if error_flag
+      reason = (xml/"jp:gist-of-correction").inner_text.toutf8
+      put_entry :key => patent_id, :value => reason, :type => "reason", :year => year, :flag => error_flag
+    end
   }
 end
 
 #ex. parse_all_xml("H16")
 def parse_and_put_all_xml(year)
+
+  file_size = Dir.entries("#{DATA_PATH}/#{year}").size - 2
+  count = 0.0
+  
   Dir.foreach("#{DATA_PATH}/#{year}") do |file_name|
     #特許公報のみ使用するためのチェック
     next if file_name == "." || file_name == ".."
     parse_and_put_xml(year, file_name)
+    count += 1
+    puts "Now #{count / file_size * 100}%"
   end
 end
 
